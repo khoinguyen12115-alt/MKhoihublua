b7c54725736dbbe14442069804edb4fed5001b09
khoinguyen12115-alt <khoinguyen12115@gmail.com>
2026-03-18 19:20:33 +0700

    Fix condition checks and update UI titles

--- script.lua
+++ script.lua
@@ -3,7 +3,7 @@ task.wait(2)
 
 local PlaceID = 4520749081
 local secondPlaceID = 6381829480
-if game.PlaceId ~= PlaceID and game.PlaceId ~= secondPlaceID then return end
+if game.PlaceId == not PlaceID or game.PlaceId == not secondPlaceID then return end
 
 local _version = "1.6.63"
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/download/" .. _version .. "/main.lua"))() 
@@ -46,7 +46,7 @@ local function autoKen()
     local kenHaki = CharacterModel.Services.KenHaki
     local kenEvent = game:GetService("ReplicatedStorage"):WaitForChild("Chest"):WaitForChild("Remotes"):WaitForChild("Functions"):WaitForChild("KenEvent")
 
-    if not kenOpen.Value then kenEvent:InvokeServer() end    
+    if not kenOpen.value then kenEvent:InvokeServer() end    
 
     kenHaki.Changed:Connect(function(kenValue)
         if (kenValue == 1 or kenValue == 8) and not kenOpen.Value then kenEvent:InvokeServer() end  
@@ -319,13 +319,14 @@ LocalPlayer.CharacterAdded:Connect(function()
     autoHaki()
 end)
 
+
 -- Gui
 
 local Window = WindUI:CreateWindow({
-    Title = "MKhoihub",
+    Title = "MKHOI",
     Icon = "door-open", 
     Author = "by ag_ma_ir",
-    Folder = "Mkhoisuperdiv",
+    Folder = "MKHOI-TAYNINH",
 
     Transparent = true,
     Resizable = true,
@@ -361,7 +362,7 @@ local Stats = Window:Tab({Title = "Stats", Locked = false,})
 
 local statsParagraph = Stats:Paragraph({Title = "Stats", Locked = false,})
 
-local untilParagraph = Stats:Paragraph({Title = "Second Sea", Desc = "", Locked = false,})
+local untilParagraph = Stats:Paragraph({Title = "Second Sea", Desc, Locked = false,})
 ReplicatedStorage:GetAttributeChangedSignal("GhostShipSpawnText"):Connect(function()
 local untilSeaMonster = ReplicatedStorage:GetAttribute("SeaMonsterSpawnText")    
 local untilGhostShip = ReplicatedStorage:GetAttribute("GhostShipSpawnText") 
@@ -390,7 +391,7 @@ local function enableAutoFarm()
 end 
 local function disableAutoFarm()
     task.cancel(Farm)
-   HumanoidRootPart.CFrame = CFrame.new(0,20,0)
+    HumanoidRootPart.CFrame = Vector3.new(0,20,0)
     HumanoidRootPart.Anchored = false
 end
 
@@ -529,7 +530,7 @@ local selectKeyDropdown = chest:Dropdown({
         "Platinum Key",
         "Diamond Key",
     },
-    Desc"", --
+    Desc,
     Value = "Key",
     Callback = function(option) 
         SelectedKey = option
